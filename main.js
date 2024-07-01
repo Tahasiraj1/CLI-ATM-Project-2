e7b0313e682b6b78efedb06e4ab4ed8f35ff8714
Tahasiraj1 <160783918+Tahasiraj1@users.noreply.github.com>
2024-07-01 10:24:11 +0500

    Add files via upload
    This somewhat complex TypeScript/Node.js project is a console-based application. When the system starts the user is prompted with a user id and user pin. After entering the details successfully, the ATM functionalities are unlocked.

---
+++ main.js
@@ -0,0 +1,58 @@
+import inquirer from 'inquirer';
+const answers = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'userID',
+        message: 'Kindly enter you ID:'
+    },
+    {
+        type: 'input',
+        name: 'userPIN',
+        message: 'Kindly enter you PIN:'
+    },
+    {
+        type: 'list',
+        name: 'accountype',
+        choices: ['Current', 'Saving'],
+        message: 'Select your AccountType: '
+    },
+    {
+        type: 'list',
+        name: 'transactiontype',
+        choices: ['Fast Cash', 'Withdraw'],
+        message: 'Select your TransactionType: ',
+        when(answers) {
+            return answers.accountype;
+        }
+    },
+    {
+        type: 'list',
+        name: 'Amount',
+        choices: ['1000', '2000', '5000', '10000'],
+        message: 'Select your Amount: ',
+        when(answers) {
+            return answers.transactiontype == 'Fast Cash';
+        }
+    },
+    {
+        type: 'number',
+        name: 'Amount',
+        message: 'Enter your amount: ',
+        when(answers) {
+            return answers.transactiontype == 'Withdraw';
+        }
+    }
+]);
+if (answers.userID && answers.userPIN) {
+    const balance = Math.floor(Math.random() * 100000000);
+    console.log(balance);
+    const EnteredAmount = answers.Amount;
+    if (balance >= EnteredAmount) {
+        const Remaining = balance - EnteredAmount;
+        console.log('Your remaining balance is: ', Remaining);
+    }
+    else {
+        console.log('Insufficient Balance');
+    }
+}
+;
